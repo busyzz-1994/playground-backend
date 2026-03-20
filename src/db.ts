80d572c8e1ba4bbdd8ab61ea2cb56f448527462d
buzzyz-1994 <busyzz123@gmail.com>
2026-03-21 00:39:32 +0800

    db migratie

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "./generated/prisma";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
+  connectionString: process.env.POSTGRES_PRISMA_URL!,
 });
 
 export const prisma = new PrismaClient({ adapter });
